82b458e98fa0b25738f2c7668103ce5e2c9120ef
GN <andreifootball@yandex.ru>
2023-03-14 12:13:32 +0500

    Ending 27

--- 27/Program.cs
+++ 27/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Sum()
+{
+    Console.Write("Введите число для которого будем искать сумму цифр внутри него: ");
+    string num = Console.ReadLine();
+    int N = int.Parse(num);
+    int result = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        result = result + N % 10;
+        N = N / 10;
+    }
+    return result;
+}
+
+Console.WriteLine(Sum());
