bcef956ef48e42b2028e34d2609ebb886716dd31
GN <andreifootball@yandex.ru>
2023-03-14 12:26:35 +0500

    Ending 29

--- 29/Program.cs
+++ 29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = new int[8];
+
+int[] fillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+    return array;
+}
+
+void printArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}]");
+    }
+}
+
+fillArray(array);
+printArray(array);
