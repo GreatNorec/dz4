f4adcb2810a4d55d4b695e73b9ded6013c716390
GN <andreifootball@yandex.ru>
2023-03-14 11:41:43 +0500

    Ending 25

--- 25/Program.cs
+++ 25/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Не совсем понимаю зачем реализовывать задачу через цикл, мне показалось проще через математическую функцию
+
+int A = 3;
+int B = 5;
+
+double MetodPow(int A, int B)
+{
+    double result = Math.Pow(A, B);
+    return result;
+}
+
+Console.WriteLine(MetodPow(A, B));
+
+//Так же решил реализовать через цикл
+
+double forPow(int A, int B)
+{
+    double result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result = result * A;
+    }
+    return result;
+}
+
+Console.WriteLine(forPow(A, B));
